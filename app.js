562ea8244be3034913eeec25af1f99fb6e2d15da
Sumit Jadiya <sumit.jadiya@turtlemint.com>
2021-12-26 21:42:18 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
+
+// for swagger documentation
+const swaggerUi = require('swagger-ui-express')
+const yaml = require('yamljs')
+const swaggerDocument = yaml.load('./swagger/swagger.yaml')
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+// regular middleware
+app.use(express.json())
+
+// morgan middleware
+app.use(morgan('tiny'))
+
+app.get('/', (req, res) => {
+  res.send('test')
+})
+
+// export app js
+module.exports = app
